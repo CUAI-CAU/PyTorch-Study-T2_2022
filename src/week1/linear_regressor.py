90f99ae02ae01d6654e8e82448036fb2fb3648ac
unanchoi <unusualc@likelion.org>
2022-03-20 12:03:29 +0900

    Feat : add Linear regressor code

---
+++ src/week1/linear_regressor.py
@@ -0,0 +1,22 @@
+import torch
+
+
+# Linear Regressor
+x_train = torch.FloatTensor([1],[2],[3])
+y_train = torch.FloatTensor([2],[4],[6])
+
+W = torch.zeros(1, requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+optimizer = torch.optim.SGD([W,b], lr = 0.01)
+
+nb_epochs = 1000
+for i in range(1, nb_epochs+1):
+    hypothesis = x_train * W + b
+    cost = torch.mean((hypothesis - y_train) ** 2)
+
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+# Gradient Decent
