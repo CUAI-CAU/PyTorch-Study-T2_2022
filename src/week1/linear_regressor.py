e5ce4877a4bb7ea4c196abf9cabacdc6f808f22f
unanchoi <unusualc@likelion.org>
2022-03-20 12:33:34 +0900

    Feat : add gradient decent practice code

--- src/week1/linear_regressor.py
+++ src/week1/linear_regressor.py
@@ -2,8 +2,8 @@ import torch
 
 
 # Linear Regressor
-x_train = torch.FloatTensor([1],[2],[3])
-y_train = torch.FloatTensor([2],[4],[6])
+x_train = torch.FloatTensor([[1],[2],[3]])
+y_train = torch.FloatTensor([[2],[4],[6]])
 
 W = torch.zeros(1, requires_grad=True)
 b = torch.zeros(1, requires_grad=True)
@@ -20,3 +20,30 @@ for i in range(1, nb_epochs+1):
     optimizer.step()
 
 # Gradient Decent
+x_train = torch.FloatTensor([[1],[2],[3]])
+y_train = torch.FloatTensor([[1],[2],[3]])
+
+W = torch.zeros(1, requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+optimizer = torch.optim.SGD([W,b], lr = 0.01)
+
+nb_epochs = 1000
+for epoch in range(1, nb_epochs+1):
+
+    #H(x) 계산
+    hypothesis = x_train * W + b
+
+    #cost gradient 계산
+    cost = torch.mean((hypothesis - y_train) ** 2)
+    gradient = torch.sum((W * x_train - y_train) * x_train)
+
+    print("epoch : {:4d}/ {} Weight : {:3f} Cost : {:6f}".format(epoch,nb_epochs, W.item(), cost.item()))
+
+    # cost H(x)로 개선
+    optimizer.zero_grad()
+    cost.backward()
+    optimizer.step()
+
+
+
